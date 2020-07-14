b178040bcc1ce471c3249dbdafb57ab4a3695d44
5FiftySix6 <ybham6@gmail.com>
2020-07-14 08:35:07 -0400

    Swapped to my tink, should fix the bug

--- FightController.cs
+++ FightController.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using Modding;
+﻿using UnityEngine;
 using System.Collections;
 using HutongGames.PlayMaker.Actions;
-using ModCommon.Util;
 using ModCommon;
-using HutongGames.PlayMaker;
-using Object = UnityEngine.Object;
-using System.Reflection;
 
 namespace FiveKnights
 {
@@ -26,9 +17,13 @@ namespace FiveKnights
         private IEnumerator Start()
         {
             Instance = this;
+            
             yield return new WaitWhile(() => !GameObject.Find("White Defender"));
+            
             _whiteD = GameObject.Find("White Defender");
+            
             //_whiteD.AddComponent<WDController>();
+            
             GameManager.instance.gameObject.AddComponent<WDController>().dd = _whiteD;
             
             PlayMakerFSM fsm = _whiteD.LocateMyFSM("Dung Defender");
@@ -37,71 +32,93 @@ namespace FiveKnights
 
             GameObject dungBall = fsm.GetAction<SpawnObjectFromGlobalPool>("Throw 1", 1).gameObject.Value;
             FiveKnights.preloadedGO["ball"] = dungBall;
-            
+
             foreach (GameObject i in Resources.FindObjectsOfTypeAll<GameObject>())
             {
-                if (i.PrintSceneHierarchyPath() == "Hollow Shade\\Slash")
-                {
-                    FiveKnights.preloadedGO["parryFX"] = i.LocateMyFSM("nail_clash_tink").GetAction<SpawnObjectFromGlobalPool>("No Box Down", 1).gameObject.Value;
-                    AudioClip aud = i.LocateMyFSM("nail_clash_tink").GetAction<AudioPlayerOneShot>("Blocked Hit", 5).audioClips[0];
-                    GameObject clashSndObj = new GameObject();
-                    AudioSource clashSnd = clashSndObj.AddComponent<AudioSource>();
-                    clashSnd.clip = aud;
-                    clashSnd.pitch = UnityEngine.Random.Range(0.85f, 1.15f);
-                    FiveKnights.preloadedGO["ClashTink"] = clashSndObj;
-                    break;
-                }
+                if (i.PrintSceneHierarchyPath() != "Hollow Shade\\Slash")
+                    continue;
+                
+                FiveKnights.preloadedGO["parryFX"] = i.LocateMyFSM("nail_clash_tink").GetAction<SpawnObjectFromGlobalPool>("No Box Down", 1).gameObject.Value;
+
+                AudioClip aud = i
+                                .LocateMyFSM("nail_clash_tink")
+                                .GetAction<AudioPlayerOneShot>("Blocked Hit", 5)
+                                .audioClips[0];
+
+                var clashSndObj = new GameObject();
+                var clashSnd = clashSndObj.AddComponent<AudioSource>();
+
+                clashSnd.clip = aud;
+                clashSnd.pitch = Random.Range(0.85f, 1.15f);
+
+                Tink.TinkClip = aud;
+
+                FiveKnights.preloadedGO["ClashTink"] = clashSndObj;
+
+                break;
             }
         }
 
         public void CreateIsma()
         {
             Log("Creating Isma");
+            
             _isma = Instantiate(FiveKnights.preloadedGO["Isma"]);
+            
             FiveKnights.preloadedGO["Isma2"] = _isma;
+            
             _isma.SetActive(true);
+            
             foreach (SpriteRenderer i in _isma.GetComponentsInChildren<SpriteRenderer>(true))
             {
                 i.material = new Material(Shader.Find("Sprites/Default"));
+                
                 if (i.name == "FrontW")
-                {
                     continue;
-                }
-                if (i.gameObject.GetComponent<PolygonCollider2D>() || i.gameObject.GetComponent<BoxCollider2D>())
-                {
-                    i.gameObject.AddComponent<DamageHero>().damageDealt = 1;
-                    i.gameObject.layer = 11;
-                }
+
+                if (!i.gameObject.GetComponent<PolygonCollider2D>() && !i.gameObject.GetComponent<BoxCollider2D>()) 
+                    continue;
+                
+                i.gameObject.AddComponent<DamageHero>().damageDealt = 1;
+                i.gameObject.layer = 11;
             }
+            
             foreach (LineRenderer lr in _isma.GetComponentsInChildren<LineRenderer>(true))
             {
                 lr.material = new Material(Shader.Find("Sprites/Default"));
             }
-            SpriteRenderer _sr = _isma.GetComponent<SpriteRenderer>();
+            
+            var _sr = _isma.GetComponent<SpriteRenderer>();
             _sr.material = ArenaFinder.materials["flash"];
+            
             _isma.AddComponent<IsmaController>();
+            
             Log("Done creating Isma");
         }
         
         public DryyaSetup CreateDryya()
         {
             _dryya = Instantiate(FiveKnights.preloadedGO["Dryya"], new Vector2(90, 25), Quaternion.identity);
+            
             return _dryya.AddComponent<DryyaSetup>();
         }
 
         public HegemolController CreateHegemol()
         {
             Log("Creating Hegemol");
-            _hegemol = Instantiate(FiveKnights.preloadedGO["fk"],
-                new Vector2(HeroController.instance.transform.position.x, 23), Quaternion.identity);
+            
+            _hegemol = Instantiate(FiveKnights.preloadedGO["fk"], new Vector2(HeroController.instance.transform.position.x, 23), Quaternion.identity);
             _hegemol.SetActive(true);
+            
             Log("Adding HegemolController component");
+            
             return _hegemol.AddComponent<HegemolController>();
         }
         
         public ZemerController CreateZemer()
         {
             Log("Creating Zemer");
+            
             _zemer = Instantiate(FiveKnights.preloadedGO["Zemer"]);
             _zemer.SetActive(true);
             
@@ -110,27 +127,38 @@ namespace FiveKnights
                 i.gameObject.AddComponent<DamageHero>().damageDealt = 1;
                 i.gameObject.layer = 22;
             }
+            
             foreach (Transform i in FiveKnights.preloadedGO["SlashBeam2"].transform)
             {
                 i.GetComponent<SpriteRenderer>().material =  new Material(Shader.Find("Sprites/Default"));   
+                
                 i.Find("HB1").gameObject.AddComponent<DamageHero>().damageDealt = 1;
                 i.Find("HB2").gameObject.AddComponent<DamageHero>().damageDealt = 1;
+                
                 i.Find("HB1").gameObject.layer = 22;
                 i.Find("HB2").gameObject.layer = 22;
             }
+            
             foreach (Transform i in FiveKnights.preloadedGO["SlashBeam3"].transform)
             {
                 i.GetComponent<SpriteRenderer>().material =  new Material(Shader.Find("Sprites/Default"));   
+                
                 i.Find("HB1").gameObject.AddComponent<DamageHero>().damageDealt = 1;
                 i.Find("HB2").gameObject.AddComponent<DamageHero>().damageDealt = 1;
+                
                 i.Find("HB1").gameObject.layer = 22;
                 i.Find("HB2").gameObject.layer = 22;
             }
+            
             foreach (SpriteRenderer i in _zemer.GetComponentsInChildren<SpriteRenderer>(true))
             {
                 i.material = new Material(Shader.Find("Sprites/Default"));
-                BoxCollider2D bc = i.gameObject.GetComponent<BoxCollider2D>();
-                if (bc == null) continue;
+                
+                var bc = i.gameObject.GetComponent<BoxCollider2D>();
+                
+                if (bc == null) 
+                    continue;
+                
                 bc.isTrigger = true;
                 bc.gameObject.AddComponent<DamageHero>().damageDealt = 1;
                 bc.gameObject.layer = 22;
@@ -140,12 +168,16 @@ namespace FiveKnights
             { 
                 i.isTrigger = true;
                 i.gameObject.AddComponent<DamageHero>().damageDealt = 1;
-                i.gameObject.AddComponent<Parryable>();
+                i.gameObject.AddComponent<Tink>();
                 i.gameObject.layer = 22;
+                
             }
-            SpriteRenderer _sr = _zemer.GetComponent<SpriteRenderer>();
-            ZemerController zc = _zemer.AddComponent<ZemerController>();
+            
+            _zemer.GetComponent<SpriteRenderer>();
+            var zc = _zemer.AddComponent<ZemerController>();
+            
             Log("Done creating Zemer");
+            
             return zc;
         }
 
