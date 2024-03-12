8448e4f413ca2086e127d5b5e9ed79b719eecf64
Hoo-Knows <73412292+Hoo-Knows@users.noreply.github.com>
2024-03-12 12:47:36 -0700

    Bump version

--- FiveKnights.cs
+++ FiveKnights.cs
@@ -199,7 +199,7 @@ namespace FiveKnights
             }
         }
 
-        public override string GetVersion() => "2024-3-7";
+        public override string GetVersion() => "1.0.1.1";
 
         public override List<(string, string)> GetPreloadNames()
         {
